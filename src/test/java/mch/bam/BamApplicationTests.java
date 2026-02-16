ff42196b3fad95d7fabd6c1299444728dde1d808
문창현 <jake123456@naver.com>
2026-02-16 18:13:04 +0900

    first commit

---
+++ src/test/java/mch/bam/BamApplicationTests.java
@@ -0,0 +1,13 @@
+package mch.bam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
